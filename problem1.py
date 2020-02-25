eadd2434e8ff94ade4c19792e9b9f5dbc6dc2217
lburr192 <60945711+lburr192@users.noreply.github.com>
2020-02-25 15:43:10 -0500

    Update problem1.py

--- problem1.py
+++ problem1.py
@@ -2,9 +2,8 @@
 from microbit import *
 import random
 
-# display.scroll("Even or odd?")
-# display.scroll("Press B for even, A for odd")
-# display.scroll("shake to reset")
+display.scroll("Even or odd?")
+display.scroll("Press B for even, A for odd")
 
 while True:
     suprise = int(random.randint(1, 20))
@@ -15,16 +14,20 @@ while True:
         if (mod == 0):
             if (button_a.is_pressed()):
                 display.show(Image.SAD)
+                sleep(5000)
                 break
             elif (button_b.is_pressed()):
                 display.show(Image.HAPPY)
+                sleep(5000)
                 break
         else:
             if (button_a.is_pressed()):
                 display.show(Image.HAPPY)
+                sleep(5000)
                 break
             elif (button_b.is_pressed()):
                 display.show(Image.SAD)
+                sleep(5000)
                 break
 
     display.scroll("Cont?")
